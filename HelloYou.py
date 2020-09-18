ac1d02acc81952b5ee5b87e7d08ac5fac4f998c9
Alex Marquez <alexmarquez@MacBook-Air-van-Alex.local>
2020-09-18 12:05:37 +0200

    Bestanden toevoegen

---
+++ HelloYou.py
@@ -0,0 +1,5 @@
+print(“Hello You!, ik ben Alex”)
+print(“Wie ben jij?”)
+input()
+print(“Hello ” + name)
+
--- Test.py
+++ Test.py
@@ -1,1 +1,4 @@
-print("Alex")
+print("Hallo ik ben Alex, wie ben jij?")
+Jouwnaam  = input ()
+print ("Hallo " +  Welkom + ", Op deze pagina")
+if anwser1 = input ()
