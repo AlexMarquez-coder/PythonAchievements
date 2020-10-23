b966e8b7e6720e98c90f4c3d45f2fa27d50c257e
Alex Marquez <alexmarquez@MacBook-Air-van-Alex.local>
2020-10-23 13:15:19 +0200

    Week10

--- HelloYou.py
+++
@@ -1,5 +0,0 @@
-print(“Hello You!, ik ben Alex”)
-print(“Wie ben jij?”)
-input()
-print(“Hello ” + name)
-
--- Test.py
+++
@@ -1,4 +0,0 @@
-print("Hallo ik ben Alex, wie ben jij?")
-Jouwnaam  = input ()
-print ("Hallo " +  Welkom + ", Op deze pagina")
-if anwser1 = input ()
